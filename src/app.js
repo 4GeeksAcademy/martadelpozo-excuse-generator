130714abcc29510d0054816d70ddd7dc60884169
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2023-10-02 19:05:12 +0000

    my excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateRandomExcuse();
 };
+
+// Excuses
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function generateRandomExcuse() {
+  let whoExcuse = who[Math.floor(Math.random() * who.length)];
+  let actionExcuse = action[Math.floor(Math.random() * action.length)];
+  let whatExcuse = what[Math.floor(Math.random() * what.length)];
+  let whenExcuse = when[Math.floor(Math.random() * when.length)];
+
+  return `${whoExcuse} ${actionExcuse} ${whatExcuse} ${whenExcuse}`;
+}
