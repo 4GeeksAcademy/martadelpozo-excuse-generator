5a439396cbd3e90cb0c30b0c3bf858bfc74a98fd
martadelpozo <138892528+martadelpozo@users.noreply.github.com>
2023-10-04 19:12:17 +0000

    Refactored Excuse Generator

--- src/app.js
+++ src/app.js
@@ -7,26 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  document.getElementById("excuse").innerHTML = generateRandomExcuse();
+  document.querySelector("#excuse").innerHTML = excusegenerator();
 };
 
-// Excuses
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+let excusegenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-function generateRandomExcuse() {
-  let whoExcuse = who[Math.floor(Math.random() * who.length)];
-  let actionExcuse = action[Math.floor(Math.random() * action.length)];
-  let whatExcuse = what[Math.floor(Math.random() * what.length)];
-  let whenExcuse = when[Math.floor(Math.random() * when.length)];
+  // let quien = Math.floor(Math.random() * who.length);
+  let quien = ObtenerElementoAleatorio(who);
+  let accion = ObtenerElementoAleatorio(action);
+  let que = ObtenerElementoAleatorio(what);
+  let cuando = ObtenerElementoAleatorio(when);
 
-  return `${whoExcuse} ${actionExcuse} ${whatExcuse} ${whenExcuse}`;
+  return (
+    who[quien] + " " + action[accion] + " " + what[que] + " " + when[cuando]
+  );
+};
+
+let refresh = document.getElementById("refresh");
+refresh.addEventListener("click", _ => {
+  location.reload();
+});
+
+function ObtenerElementoAleatorio(ArrayCalcular) {
+  console.log("ObtenerElementoAleatorio");
+
+  return Math.floor(Math.random() * ArrayCalcular.length);
 }
